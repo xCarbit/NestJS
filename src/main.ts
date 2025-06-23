277552417a383ef21d90cd7fc371095f9065e4a4
Filip <filip.sredojevic02@gmail.com>
2025-06-23 11:18:55 +0200

    Inicijalno stanje

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule); //Napravi novu NestJS aplikaciju koristeci AppModule kao route module
+  await app.listen(process.env.PORT ?? 3000); //Slusaj na portu 3000
+}
+bootstrap();
